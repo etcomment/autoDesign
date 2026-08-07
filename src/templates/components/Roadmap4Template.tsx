f46e9201d4d25bb679562be506cb077277c53c37
etcomment <stiven.guillaume@gmail.com>
2026-08-07 11:20:54 +0200

    Revert "fix(templates): origine dynamique sR.x des serpentins pour permettre le déplacement horizontal solidaire sans étirement de la flèche seule"
    This reverts commit 990c19b097f9ce642c8d837d5e424f78dc8416ca.

--- src/templates/components/Roadmap4Template.tsx
+++ src/templates/components/Roadmap4Template.tsx
@@ -282,19 +282,19 @@ export function Roadmap4Template({ data }: { data: RoadmapData }): ReactElement 
           const startY = sR.y + sR.height / 2
           pathD = `M ${startX} ${startY} L ${aR.x + 3} ${startY}`
         } else if (direction === 'left') {
-          const startX = sR.x + sR.width
-          const startY = sR.y + sR.height / 2 + rowHeight
+          const prevArrowBaseX = 470
+          const startY = sR.y + rowHeight + sR.height / 2
           const turnX = getTurnRightX(i - 1)
-          const targetY = sR.y + sR.height / 2
+          const targetY = startY - rowHeight
 
-          pathD = `M ${startX} ${startY} L ${turnX - R} ${startY} A ${R} ${R} 0 0 0 ${turnX} ${startY - R} L ${turnX} ${targetY + R} A ${R} ${R} 0 0 0 ${turnX - R} ${targetY} L ${aR.x + aR.width - 3} ${targetY}`
+          pathD = `M ${prevArrowBaseX} ${startY} L ${turnX - R} ${startY} A ${R} ${R} 0 0 0 ${turnX} ${startY - R} L ${turnX} ${targetY + R} A ${R} ${R} 0 0 0 ${turnX - R} ${targetY} L ${aR.x + aR.width - 3} ${targetY}`
         } else {
-          const startX = sR.x
-          const startY = sR.y + sR.height / 2 + rowHeight
+          const prevArrowBaseX = 555
+          const startY = sR.y + rowHeight + sR.height / 2
           const turnX = getTurnLeftX(i - 1)
-          const targetY = sR.y + sR.height / 2
+          const targetY = startY - rowHeight
 
-          pathD = `M ${startX} ${startY} L ${turnX + R} ${startY} A ${R} ${R} 0 0 1 ${turnX} ${startY - R} L ${turnX} ${targetY + R} A ${R} ${R} 0 0 1 ${turnX + R} ${targetY} L ${aR.x + 3} ${targetY}`
+          pathD = `M ${prevArrowBaseX} ${startY} L ${turnX + R} ${startY} A ${R} ${R} 0 0 1 ${turnX} ${startY - R} L ${turnX} ${targetY + R} A ${R} ${R} 0 0 1 ${turnX + R} ${targetY} L ${aR.x + 3} ${targetY}`
         }
 
         return (
