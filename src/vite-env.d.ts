600252210e077846ac5265141e6442094c42c158
Stiven <stiven.guillaume@gmail.com>
2026-08-25 18:19:53 +0200

    fix(types): add pptxgenjs module declaration in vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,7 @@
 /// <reference types="vite/client" />
+
+declare module 'pptxgenjs' {
+  import PptxGenJS from 'pptxgenjs/types/index'
+  export default PptxGenJS
+  export type * from 'pptxgenjs/types/index'
+}
